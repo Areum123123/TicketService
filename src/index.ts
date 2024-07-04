5c4e2bdfcd705361ec3799f3d59c361042693b10
kang <kang10232@naver.com>
2024-07-04 09:54:59 +0900

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 3020;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`${port}번 포트로 서버가 열렸습니다.`);
+});
